ce7f0d56666ee1f53ad21ac330de2ed6f0991f0e
SzilvasiPeter <peti.szilvasi95@gmail.com>
2019-03-27 18:49:48 +0100

    First commit and Initalizing project

---
+++ src/duck/Duck.java
@@ -0,0 +1,25 @@
+package duck;
+
+public abstract class Duck {
+    // Reference to the behaviors
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+
+    public Duck() {}
+
+    public abstract void display();
+
+    public void performFly(){
+        // Delegate behavior
+        flyBehavior.fly();
+    }
+
+    public void performQuack(){
+        // Delegate behavior
+        quackBehavior.quack();
+    }
+
+    public void swim(){
+        System.out.println("All ducks float, even decoys!");
+    }
+}
