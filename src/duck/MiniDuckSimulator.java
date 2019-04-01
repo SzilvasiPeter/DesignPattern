a8b78fde3731dca3c855b65817d2b267fd2a8b43
SzilvasiPeter <peti.szilvasi95@gmail.com>
2019-04-01 17:43:56 +0200

    Add setters to the abstract class Add new Flying class and Duck class

--- src/duck/MiniDuckSimulator.java
+++ src/duck/MiniDuckSimulator.java
@@ -6,5 +6,10 @@ public class MiniDuckSimulator {
         Duck mallard = new MallardDuck();
         mallard.performQuack();
         mallard.performFly();
+
+        Duck modelDuck = new ModelDuck();
+        modelDuck.performFly();
+        modelDuck.setFlyBehavior(new FlyRocketPowered());
+        modelDuck.performFly();
     }
 }
