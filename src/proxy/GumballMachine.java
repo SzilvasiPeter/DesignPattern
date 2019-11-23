ed98d64f08be5f94c70f6301ad2a25733a914493
SzilvasiPeter <peti.szilvasi95@gmail.com>
2019-11-23 09:02:18 +0100

    Create proxy pattern

---
+++ src/proxy/GumballMachine.java
@@ -0,0 +1,13 @@
+import proxy;
+
+public class GumballMachine {
+    private String location;
+
+    public GumballMachine(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+}
---
+++ src/proxy/GumballMonitor.java
@@ -0,0 +1,9 @@
+import proxy;
+
+public class GumballMonitor {
+    private GumballMachine gumballMachine;
+
+    public GumballMonitor(GumballMachine gumballMachine){
+        this.gumballMachine = gumballMachine;
+    }
+}
