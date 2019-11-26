5f0587baccb50afbe668ff46bf327494dd3ab543
SzilvasiPeter <peti.szilvasi95@gmail.com>
2019-11-26 20:28:13 +0100

    GumballMonitor client was extended with report method

--- src/proxy/gumball/GumballMonitor.java
+++ src/proxy/gumball/GumballMonitor.java
@@ -1,10 +1,21 @@
 package proxy.gumball;
 
+import java.rmi.*;
 
 public class GumballMonitor {
     private GumballMachine gumballMachine;
 
-    public GumballMonitor(GumballMachine gumballMachine){
+    public GumballMonitor(GumballMachine gumballMachine) {
         this.gumballMachine = gumballMachine;
+    }
+
+    public void report() {
+        try {
+            System.out.println("Gumball Machine: " + gumballMachine.getLocation());
+            System.out.println("Current inventory: " + gumballMachine.getCount() + " gumballs");
+            System.out.println("Current state: " + gumballMachine.getState());
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 }
