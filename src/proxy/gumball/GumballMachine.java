78ca2b225797a95585d842a28ff33bfe8a4d6339
SzilvasiPeter <peti.szilvasi95@gmail.com>
2019-11-26 20:16:14 +0100

    Create interface providing methods, which can be called remotely

--- src/proxy/gumball/GumballMachine.java
+++ src/proxy/gumball/GumballMachine.java
@@ -1,4 +1,4 @@
-package proxy;
+package proxy.gumball;
 
 public class GumballMachine {
     private String location;
--- src/proxy/gumball/GumballMonitor.java
+++ src/proxy/gumball/GumballMonitor.java
@@ -1,4 +1,4 @@
-package proxy;
+package proxy.gumball;
 
 
 public class GumballMonitor {
