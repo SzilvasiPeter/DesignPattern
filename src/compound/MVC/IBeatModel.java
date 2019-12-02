c72554f80948f732b9cf2a72d94626bc3adf99d4
SzilvasiPeter <peti.szilvasi95@gmail.com>
2019-12-02 20:29:41 +0100

    In Model-View-Controller create our model interface

---
+++ src/compound/MVC/IBeatModel.java
@@ -0,0 +1,12 @@
+package compound.MVC;
+
+public interface IBeatModel {
+    void on();
+    void off();
+    void setBPM(int bpm);
+    int getBPM();
+    void registerObserver(BeatObserver beatObserver);
+    void removeObserver(BeatObserver beatObserver);
+    void registerObserver(BMPObserver bmpObserver);
+    void removeObserver(BMPObserver bmpObserver);
+}
